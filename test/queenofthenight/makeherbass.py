77fab54b323be3129ef68d38f99c4a9e317d3a37
jackruder <jackruder14@gmail.com>
2023-03-21 22:08:57 -0700

    she sings in bass now

--- test/queenofthenight/makeherbass.py
+++ test/queenofthenight/makeherbass.py
@@ -1,21 +1,18 @@
 import numpy as np
-from scipy import signal
+from scipy import signal, fft
 import matplotlib.pyplot as plt
-from numericalmethods import *
 import rustlets
 import scipy.io.wavfile as wvf
-
-def fwavelength(w_0):
-    return 4 * np.pi / (w_0 + np.sqrt(2 + w_0**2))
+import cmath
 
 def main():
-    FILENAME = "vocals.wav"
+    FILENAME = "queenaudio.wav"
     hz, data = wvf.read(FILENAME)
 
-    log_steps = 10
+    log_steps = 16
 
-    num_seconds = 0.5
-    start = 7.5
+    num_seconds = 10
+    start = 7
 
     octave_shift = 3
 
@@ -25,43 +22,39 @@ def main():
 
     print("Audio Sample Rate: ", hz)
 
-    #plt.plot(times,signal,label="original")
-    #plt.show()
+    plt.plot(times,signal,label="original")
+    plt.show()
 
     ## do the morlet transform
     print("Computing CWT...")
-    cwt_list,scales_list = rustlets.cwt_morlet(signal,hz,log_steps,True)
 
-    print("Done.")
-
+    cwt_list,scales_list = rustlets.cwt_morlet_ext(signal,hz,log_steps, False)
     scales = np.array(scales_list)
-    freqs = 1 / (scales * fwavelength(2 * np.pi))
+    freqs = 1 / (scales * rustlets.morlet_wavelength(2 * np.pi))
     cwtmat = np.array(cwt_list,dtype=np.complex64).reshape((len(scales),-1))[:,:len(times)]
 
-    newcwtmat = np.zeros(cwtmat.shape,dtype=np.complex64)
-    for i in range(octave_shift * log_steps,cwtmat.shape[0]):
-        newcwtmat[i] = cwtmat[i-(octave_shift * log_steps)]
-
-    #plt.pcolormesh(times,freqs, np.abs(newcwtmat), cmap='viridis', shading='gouraud')
-    #plt.show()
-##
-
-    newcwtflat = newcwtmat.flatten()
-
-    print("Computing iCWT...")
-    f_rec = np.array(rustlets.icwt_morlet(newcwtflat,scales,times)) * np.pi
     print("Done.")
 
-    ratio = max(f_rec)/max(signal)
-    f_rec = f_rec / ratio
+    rescale = 0.5
 
-    plt.plot(times,signal,label="original")
+    cwtabs = np.abs(cwtmat)
+    cwtphase = np.unwrap(np.arctan2(cwtmat.imag, cwtmat.real))
+
+    cscale = cwtabs * np.exp(cwtphase * 1j * rescale)
+    scales = scales / rescale
+
+
+    print("Computing iCWT on transformed CWT matrix...")
+    f_rec = np.array(rustlets.icwt_morlet(cscale.flatten(),scales,times))
+    print("Done. Output in bassbitch.wav")
+
+    #plt.plot(times,signal,label="original")
     plt.plot(times,f_rec,label="recovered")
     plt.legend()
     plt.show()
+    #print(np.sqrt(sum((f_rec - signal)**2)/len(signal)))
 
-    wvf.write("bassbitch.wav", hz, f_rec.astype(np.int32))
-    #wvf.write("bassbitch.wav", hz, signal)
+    wvf.write("bassbitch.wav", hz, f_rec.astype(np.int16))
 
 
 if __name__ == "__main__":
