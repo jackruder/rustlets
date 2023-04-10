ad91f4a764addd0a5a7a0210bbfcec9fd45c2c88
Jack Ruder <jackruder14@gmail.com>
2023-04-10 14:43:59 -0700

    update example to use numpy input output to cwt

--- cwtfuncs.py
+++ cwtfuncs.py
@@ -14,20 +14,18 @@ def main():
     #samples = samples + signal.chirp(times, f0=20000, f1=8000, t1=1, method='linear')
 
 
-    cwt_list,scales_list = rustlets.cwt_morlet(samples,hz,10)
+    cwtmat,scales = rustlets.cwt_morlet(samples,hz,10)
 
-    scales = np.array(scales_list)
     freqs = 1 / (scales * rustlets.morlet_wavelength(2 * np.pi))
-    cwtmat = np.array(cwt_list,dtype=np.complex64).reshape((len(scales),-1))[:,:len(times)]
 
     plt.plot(times,samples,label="original")
     plt.show()
 
-    #plt.pcolormesh(times,freqs, np.abs(cwtmat), cmap='viridis', shading='gouraud')
-    #plt.show()
+    plt.pcolormesh(times,freqs, np.abs(cwtmat), cmap='viridis', shading='gouraud')
+    plt.show()
 
 
-    f_rec = np.array(rustlets.icwt_morlet(cwt_list,scales,times))
+    #f_rec = np.array(rustlets.icwt_morlet(cwt_list,scales,times))
 
     plt.plot(times,samples,label="original")
     plt.plot(times,f_rec,label="recovered")
