94442bc2ac41d9b21356283d361321e44fd277d4
Jack Ruder <jackruder14@gmail.com>
2023-04-10 17:34:06 -0700

    show some fun things to do with scales for more efficient (usually) computation

--- cwtfuncs.py
+++ cwtfuncs.py
@@ -4,25 +4,39 @@ import matplotlib.pyplot as plt
 import matplotlib
 import rustlets
 
+def filter_scales(scales, min_freq, f_wl):
+    allfreqs = 1 / (scales * f_wl)
+    freqs = []
+    for af in allfreqs:
+        if af > min_freq:
+            freqs.append(af)
+    freqs = np.array(freqs)
+    return (1 / (freqs * f_wl), freqs)
 
 def main():
-    hz=10180
+    hz=44100
     length = 1.0
     times = np.arange(0,length,1/hz)
     samples = signal.chirp(times, f0=2200, f1=1500, t1=1, method='linear')
     samples = samples + signal.chirp(times, f0=600, f1=1100, t1=1, method='linear') + signal.gausspulse(times,fc=700)
     #samples = samples + signal.chirp(times, f0=20000, f1=8000, t1=1, method='linear')
 
-    scales = rustlets.gen_scales(length, hz, 8)
-    cwtmat = rustlets.cwt_morlet(samples,hz,scales)
+    scales = rustlets.gen_scales(length, hz, 4)
+    print(len(scales))
 
-    freqs = 1 / (scales * rustlets.morlet_wavelength(2 * np.pi))
+    low = 40 # hz cut off
+    f_wl = rustlets.morlet_wavelength(2 * np.pi)
+
+    scales, freqs = filter_scales(scales, low, f_wl)
+    print(len(scales))
+
+    cwtmat = rustlets.cwt_morlet(samples,hz,scales)
 
     plt.plot(times,samples,label="original")
     plt.show()
 
-    plt.pcolormesh(times,freqs, np.abs(cwtmat), cmap='viridis', shading='gouraud')
-    plt.show()
+    #plt.pcolormesh(times,freqs, np.abs(cwtmat), cmap='viridis', shading='gouraud')
+    #plt.show()
 
     f_rec = rustlets.icwt_morlet(cwtmat,scales,times)
 
